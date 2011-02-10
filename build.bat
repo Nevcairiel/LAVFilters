928e132a6866e21ed307c8e2c12108097df7088d
Hendrik Leppkes <h.leppkes@gmail.com>
2011-02-10 16:49:44 +0100

    Fix build.bat after project rename.

--- build.bat
+++ build.bat
@@ -3,9 +3,9 @@
 call "%VS100COMNTOOLS%vsvars32.bat"
 
 sh build_ff_win32.sh
-devenv LAVFSplitter.sln /Build "Release|Win32"
+devenv LAVFilters.sln /Build "Release|Win32"
 
 sh build_ff_x64.sh
-devenv LAVFSplitter.sln /Build "Release|x64"
+devenv LAVFilters.sln /Build "Release|x64"
 
 PAUSE
