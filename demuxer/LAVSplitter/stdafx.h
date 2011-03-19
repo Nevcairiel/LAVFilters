da74026a6a402dc1940392cadc5ac0b6c8616e0e
Hendrik Leppkes <h.leppkes@gmail.com>
2011-03-19 14:17:14 +0100

    Add linker options to load the version 6.0 common controls.

--- demuxer/LAVSplitter/stdafx.h
+++ demuxer/LAVSplitter/stdafx.h
@@ -25,6 +25,9 @@
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRALEAN
 
+// Support for Version 6.0 styles
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 // include headers
 #include <Windows.h>
 #include <Commctrl.h>
