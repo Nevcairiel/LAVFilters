87730eae9bcaa5c22a60d14fbcff4c849c10c2d2
Hendrik Leppkes <h.leppkes@gmail.com>
2011-03-19 17:02:29 +0100

    Added version information to the resources.

---
+++ common/includes/version.h
@@ -0,0 +1,36 @@
+/*
+ *      Copyright (C) 2011 Hendrik Leppkes
+ *      http://www.1f0.de
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ */
+
+#pragma once
+
+#define LAV_VERSION_MAJOR 0
+#define LAV_VERSION_MINOR 19
+
+#define LAV_AUDIO "LAV Audio Decoder"
+#define LAV_SPLITTER "LAV Splitter"
+
+/////////////////////////////////////////////////////////
+#define DO_MAKE_STR(x) #x
+#define MAKE_STR(x) DO_MAKE_STR(x)
+
+#define LAV_VERSION LAV_VERSION_MAJOR.LAV_VERSION_MINOR
+#define LAV_VERSION_TAG LAV_VERSION_MAJOR, LAV_VERSION_MINOR
+
+#define LAV_VERSION_STR MAKE_STR(LAV_VERSION)
