cdaceddc1390f3c88d263f0eafccb194ce4caa25
Hendrik Leppkes <h.leppkes@gmail.com>
2011-04-07 20:36:23 +0200

    Update version number.

--- common/includes/version.h
+++ common/includes/version.h
@@ -21,7 +21,7 @@
 #pragma once
 
 #define LAV_VERSION_MAJOR 0
-#define LAV_VERSION_MINOR 20
+#define LAV_VERSION_MINOR 21
 
 #define LAV_AUDIO "LAV Audio Decoder"
 #define LAV_SPLITTER "LAV Splitter"
