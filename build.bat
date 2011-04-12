492801a8d4a912f3e57d557d1fb9d8e3264b4211
Hendrik Leppkes <h.leppkes@gmail.com>
2011-04-12 17:56:46 +0200

    The build.bat should do a full rebuild, to avoid issues.

--- build.bat
+++ build.bat
@@ -3,9 +3,9 @@
 call "%VS100COMNTOOLS%vsvars32.bat"
 
 sh build_ff_win32.sh
-devenv LAVFilters.sln /Build "Release|Win32"
+devenv LAVFilters.sln /Rebuild "Release|Win32"
 
 sh build_ff_x64.sh
-devenv LAVFilters.sln /Build "Release|x64"
+devenv LAVFilters.sln /Rebuild "Release|x64"
 
 PAUSE
