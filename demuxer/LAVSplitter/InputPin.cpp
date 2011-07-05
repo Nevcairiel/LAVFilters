11a7822fd4e4c3cf588358b6d2a0bfec86a79e53
Hendrik Leppkes <h.leppkes@gmail.com>
2011-07-05 16:36:21 +0200

    Increase read buffer size when interfacing with the IAsyncReader for better IO performance.

--- demuxer/LAVSplitter/InputPin.cpp
+++ demuxer/LAVSplitter/InputPin.cpp
@@ -23,7 +23,7 @@
 
 #include "LAVSplitter.h"
 
-#define READ_BUFFER_SIZE 4096
+#define READ_BUFFER_SIZE 32768
 
 
 CLAVInputPin::CLAVInputPin(TCHAR *pName, CLAVSplitter *pFilter, CCritSec *pLock, HRESULT *phr)
