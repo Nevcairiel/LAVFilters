4999aa63e66f957c777f99c338505599c4b1c559
Hendrik Leppkes <h.leppkes@gmail.com>
2011-07-18 13:58:38 +0200

    If the bitstream does not provide an aspect ratio, stick to the one provided by the source filter.

--- decoder/LAVVideo/LAVVideo.cpp
+++ decoder/LAVVideo/LAVVideo.cpp
@@ -363,7 +363,8 @@ HRESULT CLAVVideo::ReconnectOutput(int width, int height, AVRational ar)
   int num = width, den = height;
   av_reduce(&num, &den, (int64_t)ar.num * num, (int64_t)ar.den * den, 255);
   if (num == 0 || den == 0) {
-    num = den = 0;
+    num = vih2->dwPictAspectRatioX;
+    den = vih2->dwPictAspectRatioY;
   }
 
   if (vih2->rcSource.right != width
