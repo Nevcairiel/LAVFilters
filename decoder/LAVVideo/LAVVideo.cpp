03461a8318b97f8d87d52de116053543f746da65
Hendrik Leppkes <h.leppkes@gmail.com>
2011-07-23 13:18:49 +0200

    Add reference info into the frame debug output.

--- decoder/LAVVideo/LAVVideo.cpp
+++ decoder/LAVVideo/LAVVideo.cpp
@@ -709,7 +709,7 @@ HRESULT CLAVVideo::Decode(const BYTE *pDataIn, int nSize, REFERENCE_TIME& rtStar
       rtStop = rtStart + (duration * (m_pFrame->repeat_pict ? 3 : 2)  / 2);
     }
 
-    DbgLog((LOG_TRACE, 10, L"Frame, rtStart: %I64d, diff: %I64d, key: %d, type: %c, repeat: %d, tidx: %d", rtStart, rtStart-m_rtPrevStart, m_pFrame->key_frame, av_get_picture_type_char(m_pFrame->pict_type), m_pFrame->repeat_pict, m_CurrentThread));
+    DbgLog((LOG_TRACE, 10, L"Frame, rtStart: %I64d, diff: %I64d, key: %d, ref: %d, type: %c, repeat: %d, tidx: %d", rtStart, rtStart-m_rtPrevStart, m_pFrame->key_frame, m_pFrame->reference, av_get_picture_type_char(m_pFrame->pict_type), m_pFrame->repeat_pict, m_CurrentThread));
 
     m_rtPrevStart = rtStart;
     m_rtPrevStop = rtStop;
