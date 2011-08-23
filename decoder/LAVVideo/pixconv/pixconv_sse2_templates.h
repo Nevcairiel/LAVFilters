b674c2110c6eedf295408240adc7bd6b5f19415d
Hendrik Leppkes <h.leppkes@gmail.com>
2011-08-23 21:46:36 +0200

    Cleanup the SSE2 templates some.

--- decoder/LAVVideo/pixconv/pixconv_sse2_templates.h
+++ decoder/LAVVideo/pixconv/pixconv_sse2_templates.h
@@ -21,7 +21,7 @@
 #pragma once
 
 #define PIXCONV_LOAD_DITHER_COEFFS(reg,line,name)  \
-  const uint16_t *name = dither_8x8_256[line % 8]; \
+  const uint16_t *name = dither_8x8_256[(line) % 8]; \
   reg = _mm_load_si128((const __m128i *)name);
 
 // Load 8 16-bit pixels into a register, and dither them to 8 bit
@@ -31,10 +31,10 @@
 // src   - memory pointer of the source
 // shift - shift offset to 16-bit (ie. 6 for 10bit)
 #define PIXCONV_LOAD_PIXEL16_DITHER(reg,dreg,src,shift)              \
-  reg = _mm_load_si128((const __m128i *)src); /* load (aligned) */   \
+  reg = _mm_load_si128((const __m128i *)(src)); /* load (aligned) */   \
   reg = _mm_slli_epi16(reg, shift);           /* shift to 16-bit */  \
   reg = _mm_adds_epu16(reg, dreg);            /* dither */           \
-  reg = _mm_srli_epi16 (reg, 8);              /* shift to 8-bit */
+  reg = _mm_srli_epi16(reg, 8);               /* shift to 8-bit */
 
 // Load 8 16-bit pixels into a register, and dither them to 8 bit
 // The 8-bit pixels will be in the 8 low-bytes in the register
@@ -50,17 +50,17 @@
 // reg   - register to store pixels in
 // src   - memory pointer of the source
 #define PIXCONV_LOAD_PIXEL8(reg,src) \
-  reg = _mm_loadu_si128((const __m128i *)src);     /* load (unaligned) */
+  reg = _mm_loadu_si128((const __m128i *)(src));     /* load (unaligned) */
 
 // Load 16 8-bit pixels into a register, using aligned memory access
 // reg   - register to store pixels in
 // src   - memory pointer of the source
 #define PIXCONV_LOAD_PIXEL8_ALIGNED(reg,src) \
-  reg = _mm_load_si128((const __m128i *)src);      /* load (aligned) */
+  reg = _mm_load_si128((const __m128i *)(src));      /* load (aligned) */
 
 // Load 8 16-bit pixels into a register, using aligned memory access
 // reg   - register to store pixels in
 // src   - memory pointer of the source
 #define PIXCONV_LOAD_PIXEL16(reg,src,shift)                             \
-  reg = _mm_load_si128((const __m128i *)src);  /* load (aligned) */     \
+  reg = _mm_load_si128((const __m128i *)(src));  /* load (aligned) */     \
   reg = _mm_slli_epi16(reg, shift);            /* shift to 16-bit */
