Nevcairiel/LAVFilters

commit a17c43c29c9d61a96abd93599cddb06a122ede7c

Move buffer processing into the decode functions for more flexibility.

decoder/LAVAudio/DTSDecoder.cpp
@@ -264,14 +264,28 @@ static void DTSRemapOutputChannels(BufferDetails *buffer, DTSHeader header)
   }
 }
 
-HRESULT CLAVAudio::DecodeDTS(const BYTE * const p, int buffsize, int &consumed, BufferDetails *out)
+int CLAVAudio::SafeDTSDecode(BYTE *pInput, int len, BYTE *pOutput, int unk1, int unk2, int *pBitdepth, int *pChannels, int *pCoreSampleRate, int *pUnk4, int *pHDSampleRate, int *pUnk5, int *pProfile)
 {
-  CheckPointer(out, E_POINTER);
+  int nPCMLen = 0;
+  __try {
+    m_pDTSDecoderContext->pDtsDecode(m_pDTSDecoderContext->dtsContext, pInput, len, pOutput, unk1, unk2, pBitdepth, pChannels, pCoreSampleRate, pUnk4, pHDSampleRate, pUnk5, pProfile);
+  } __except(EXCEPTION_EXECUTE_HANDLER) {
+    DbgLog((LOG_TRACE, 50, L"::Decode() - DTS Decoder threw an exception"));
+    nPCMLen = 0;
+    FlushDTSDecoder(TRUE);
+  }
+  return nPCMLen;
+};
+
+HRESULT CLAVAudio::DecodeDTS(const BYTE * const p, int buffsize, int &consumed, HRESULT *hrDeliver)
+{
   int nPCMLength	= 0;
   const BYTE *pDataInBuff = p;
 
   BOOL bEOF = (buffsize == -1);
   if (buffsize == -1) buffsize = 1;
+
+  BufferDetails out;
 
   consumed = 0;
   while (buffsize > 0) {
@@ -324,29 +338,17 @@ HRESULT CLAVAudio::DecodeDTS(const BYTE * const p, int buffsize, int &consumed, 
 
       int bitdepth = 0, channels = 0, CoreSampleRate = 0, HDSampleRate = 0, profile = 0;
       int unk4 = 0, unk5 = 0;
-      __try {
-        nPCMLength = m_pDTSDecoderContext->pDtsDecode(m_pDTSDecoderContext->dtsContext, m_pFFBuffer, pOut_size, m_pPCMData, 0, 8, &bitdepth, &channels, &CoreSampleRate, &unk4, &HDSampleRate, &unk5, &profile);
-      } __except(EXCEPTION_EXECUTE_HANDLER) {
-        DbgLog((LOG_TRACE, 50, L"::Decode() - DTS Decoder threw an exception"));
-        nPCMLength = 0;
-        FlushDTSDecoder(TRUE);
-      }
+      nPCMLength = SafeDTSDecode(m_pFFBuffer, pOut_size, m_pPCMData, 0, 8, &bitdepth, &channels, &CoreSampleRate, &unk4, &HDSampleRate, &unk5, &profile);
       if (nPCMLength > 0 && bitdepth != m_DTSBitDepth) {
         int decodeBits = bitdepth > 16 ? 24 : 16;
 
         // If the bit-depth changed, instruct the DTS Decoder to decode to the new bit depth, and decode the previous sample again.
-        if (decodeBits != m_DTSBitDepth && out->bBuffer->GetCount() == 0) {
+        if (decodeBits != m_DTSBitDepth && out.bBuffer->GetCount() == 0) {
           DbgLog((LOG_TRACE, 20, L"::Decode(): The DTS decoder indicated that it outputs %d bits, changing config to %d bits to compensate", bitdepth, decodeBits));
           m_DTSBitDepth = decodeBits;
 
           FlushDTSDecoder();
-          __try {
-            nPCMLength = m_pDTSDecoderContext->pDtsDecode(m_pDTSDecoderContext->dtsContext, m_pFFBuffer, pOut_size, m_pPCMData, 0, 2, &bitdepth, &channels, &CoreSampleRate, &unk4, &HDSampleRate, &unk5, &profile);
-          } __except(EXCEPTION_EXECUTE_HANDLER) {
-            DbgLog((LOG_TRACE, 50, L"::Decode() - DTS Decoder threw an exception"));
-            nPCMLength = 0;
-            FlushDTSDecoder(TRUE);
-          }
+          nPCMLength = SafeDTSDecode(m_pFFBuffer, pOut_size, m_pPCMData, 0, 2, &bitdepth, &channels, &CoreSampleRate, &unk4, &HDSampleRate, &unk5, &profile);
         }
       }
       if (nPCMLength <= 0) {
@@ -357,11 +359,11 @@ HRESULT CLAVAudio::DecodeDTS(const BYTE * const p, int buffsize, int &consumed, 
 
       m_bUpdateTimeCache = TRUE;
 
-      out->wChannels        = channels;
-      out->dwSamplesPerSec  = HDSampleRate;
-      out->sfFormat         = m_DTSBitDepth == 24 ? SampleFormat_24 : SampleFormat_16;
-      out->dwChannelMask    = get_channel_mask(channels); // TODO
-      out->wBitsPerSample   = bitdepth;
+      out.wChannels        = channels;
+      out.dwSamplesPerSec  = HDSampleRate;
+      out.sfFormat         = m_DTSBitDepth == 24 ? SampleFormat_24 : SampleFormat_16;
+      out.dwChannelMask    = get_channel_mask(channels); // TODO
+      out.wBitsPerSample   = bitdepth;
 
       // DTS Express
       if (profile == 0 && !m_bsParser.m_DTSHeader.HasCore) {
@@ -384,23 +386,27 @@ HRESULT CLAVAudio::DecodeDTS(const BYTE * const p, int buffsize, int &consumed, 
 
     // Append to output buffer
     if (nPCMLength > 0) {
-      out->bBuffer->Append(m_pPCMData, nPCMLength);
+      out.bBuffer->Append(m_pPCMData, nPCMLength);
     }
   }
 
-  if (out->bBuffer->GetCount() <= 0) {
+  if (out.bBuffer->GetCount() <= 0) {
     return S_FALSE;
   }
 
-  out->nSamples = out->bBuffer->GetCount() / get_byte_per_sample(out->sfFormat) / out->wChannels;
+  out.nSamples = out.bBuffer->GetCount() / get_byte_per_sample(out.sfFormat) / out.wChannels;
 
   if (m_pAVCtx->profile != (1 << 7)) {
-    DTSRemapOutputChannels(out, m_bsParser.m_DTSHeader);
+    DTSRemapOutputChannels(&out, m_bsParser.m_DTSHeader);
   }
-  m_pAVCtx->channels = out->wChannels;
+  m_pAVCtx->channels = out.wChannels;
 
-  m_DecodeFormat = out->sfFormat;
-  m_DecodeLayout = out->dwChannelMask;
+  m_DecodeFormat = out.sfFormat;
+  m_DecodeLayout = out.dwChannelMask;
+
+  if (SUCCEEDED(PostProcess(&out))) {
+    *hrDeliver = QueueOutput(out);
+  }
 
   return S_OK;
 }

decoder/LAVAudio/LAVAudio.cpp
@@ -1325,23 +1325,17 @@ HRESULT CLAVAudio::ProcessBuffer(BOOL bEOF)
   } else {
     // Decoding
     // Consume the buffer data
-    BufferDetails output_buffer;
     if (m_pDTSDecoderContext)
-      hr2 = DecodeDTS(p, buffer_size, consumed, &output_buffer);
+      hr2 = DecodeDTS(p, buffer_size, consumed, &hr);
     else
-      hr2 = Decode(p, buffer_size, consumed, &output_buffer);
-    // S_OK means we actually have data to process
-    if (hr2 == S_OK) {
-      if (SUCCEEDED(PostProcess(&output_buffer))) {
-        hr = QueueOutput(output_buffer);
-      }
+      hr2 = Decode(p, buffer_size, consumed, &hr);
     // FAILED - throw away the data
-    } else if (FAILED(hr2)) {
+    if (FAILED(hr2)) {
       DbgLog((LOG_TRACE, 10, L"Dropped invalid sample in ProcessBuffer"));
       m_buff.SetSize(0);
       m_bQueueResync = TRUE;
       return S_FALSE;
-    } else {
+    } else if (hr2 == S_FALSE) {
       DbgLog((LOG_TRACE, 10, L"::Decode returned S_FALSE"));
       hr = S_FALSE;
     }
@@ -1364,9 +1358,8 @@ HRESULT CLAVAudio::ProcessBuffer(BOOL bEOF)
   return hr;
 }
 
-HRESULT CLAVAudio::Decode(const BYTE * const p, int buffsize, int &consumed, BufferDetails *out)
+HRESULT CLAVAudio::Decode(const BYTE * const p, int buffsize, int &consumed, HRESULT *hrDeliver)
 {
-  CheckPointer(out, E_POINTER);
   int nPCMLength	= 0;
   const BYTE *pDataInBuff = p;
 
@@ -1375,6 +1368,8 @@ HRESULT CLAVAudio::Decode(const BYTE * const p, int buffsize, int &consumed, Buf
 
   AVPacket avpkt;
   av_init_packet(&avpkt);
+
+  BufferDetails out;
 
   consumed = 0;
   while (buffsize > 0) {
@@ -1466,23 +1461,23 @@ HRESULT CLAVAudio::Decode(const BYTE * const p, int buffsize, int &consumed, Buf
 
     // Channel re-mapping and sample format conversion
     if (nPCMLength > 0) {
-      const DWORD idx_start = out->bBuffer->GetCount();
+      const DWORD idx_start = out.bBuffer->GetCount();
 
-      out->wChannels = m_pAVCtx->channels;
-      out->dwSamplesPerSec = m_pAVCtx->sample_rate;
+      out.wChannels = m_pAVCtx->channels;
+      out.dwSamplesPerSec = m_pAVCtx->sample_rate;
       if (m_pAVCtx->channel_layout)
-        out->dwChannelMask = (DWORD)m_pAVCtx->channel_layout;
+        out.dwChannelMask = (DWORD)m_pAVCtx->channel_layout;
       else
-        out->dwChannelMask = get_channel_mask(out->wChannels);
+        out.dwChannelMask = get_channel_mask(out.wChannels);
 
       switch (m_pAVCtx->sample_fmt) {
       case AV_SAMPLE_FMT_U8:
-        out->bBuffer->Append(m_pPCMData, nPCMLength);
-        out->sfFormat = SampleFormat_U8;
+        out.bBuffer->Append(m_pPCMData, nPCMLength);
+        out.sfFormat = SampleFormat_U8;
         break;
       case AV_SAMPLE_FMT_S16:
-        out->bBuffer->Append(m_pPCMData, nPCMLength);
-        out->sfFormat = SampleFormat_16;
+        out.bBuffer->Append(m_pPCMData, nPCMLength);
+        out.sfFormat = SampleFormat_16;
         break;
       case AV_SAMPLE_FMT_S32:
         {
@@ -1501,9 +1496,9 @@ HRESULT CLAVAudio::Decode(const BYTE * const p, int buffsize, int &consumed, Buf
           const short skip = 4 - bytes_per_sample;
 
           const DWORD size = (nPCMLength >> 2) * bytes_per_sample;
-          out->bBuffer->SetSize(idx_start + size);
+          out.bBuffer->SetSize(idx_start + size);
           // We use BYTE instead of int32_t because we don't know if its actually a 32-bit value we want to write
-          BYTE *pDataOut = (BYTE *)(out->bBuffer->Ptr() + idx_start);
+          BYTE *pDataOut = (BYTE *)(out.bBuffer->Ptr() + idx_start);
 
           // The source is always in 32-bit values
           const size_t num_elements = nPCMLength / sizeof(int32_t) / m_pAVCtx->channels;
@@ -1519,18 +1514,18 @@ HRESULT CLAVAudio::Decode(const BYTE * const p, int buffsize, int &consumed, Buf
             }
           }
 
-          out->sfFormat = bits_per_sample == 32 ? SampleFormat_32 : (bits_per_sample == 24 ? SampleFormat_24 : SampleFormat_16);
-          out->wBitsPerSample = m_pAVCtx->bits_per_raw_sample;
+          out.sfFormat = bits_per_sample == 32 ? SampleFormat_32 : (bits_per_sample == 24 ? SampleFormat_24 : SampleFormat_16);
+          out.wBitsPerSample = m_pAVCtx->bits_per_raw_sample;
         }
         break;
       case AV_SAMPLE_FMT_FLT:
-        out->bBuffer->Append(m_pPCMData, nPCMLength);
-        out->sfFormat = SampleFormat_FP32;
+        out.bBuffer->Append(m_pPCMData, nPCMLength);
+        out.sfFormat = SampleFormat_FP32;
         break;
       case AV_SAMPLE_FMT_DBL:
         {
-          out->bBuffer->SetSize(idx_start + (nPCMLength / 2));
-          float *pDataOut = (float *)(out->bBuffer->Ptr() + idx_start);
+          out.bBuffer->SetSize(idx_start + (nPCMLength / 2));
+          float *pDataOut = (float *)(out.bBuffer->Ptr() + idx_start);
 
           const size_t num_elements = nPCMLength / sizeof(double) / m_pAVCtx->channels;
           for (size_t i = 0; i < num_elements; ++i) {
@@ -1540,7 +1535,7 @@ HRESULT CLAVAudio::Decode(const BYTE * const p, int buffsize, int &consumed, Buf
             }
           }
         }
-        out->sfFormat = SampleFormat_FP32;
+        out.sfFormat = SampleFormat_FP32;
         break;
       default:
         assert(FALSE);
@@ -1549,13 +1544,17 @@ HRESULT CLAVAudio::Decode(const BYTE * const p, int buffsize, int &consumed, Buf
     }
   }
 
-  if (out->bBuffer->GetCount() <= 0) {
+  if (out.bBuffer->GetCount() <= 0) {
     return S_FALSE;
   }
 
-  out->nSamples = out->bBuffer->GetCount() / get_byte_per_sample(out->sfFormat) / out->wChannels;
-  m_DecodeFormat = out->sfFormat;
-  m_DecodeLayout = out->dwChannelMask;
+  out.nSamples = out.bBuffer->GetCount() / get_byte_per_sample(out.sfFormat) / out.wChannels;
+  m_DecodeFormat = out.sfFormat;
+  m_DecodeLayout = out.dwChannelMask;
+
+  if (SUCCEEDED(PostProcess(&out))) {
+    *hrDeliver = QueueOutput(out);
+  }
 
   return S_OK;
 }

decoder/LAVAudio/LAVAudio.h
@@ -174,7 +174,7 @@ private:
   CMediaType CreateMediaType(LAVAudioSampleFormat outputFormat, DWORD nSamplesPerSec, WORD nChannels, DWORD dwChannelMask, WORD wBitsPerSample = 0) const;
   HRESULT ReconnectOutput(long cbBuffer, CMediaType& mt);
   HRESULT ProcessBuffer(BOOL bEOF = FALSE);
-  HRESULT Decode(const BYTE *p, int buffsize, int &consumed, BufferDetails *out);
+  HRESULT Decode(const BYTE *p, int buffsize, int &consumed, HRESULT *hrDeliver);
   HRESULT PostProcess(BufferDetails *buffer);
   HRESULT GetDeliveryBuffer(IMediaSample **pSample, BYTE **pData);
 
@@ -206,7 +206,8 @@ private:
   HRESULT InitDTSDecoder();
   HRESULT FreeDTSDecoder();
   HRESULT FlushDTSDecoder(BOOL bReopen = FALSE);
-  HRESULT DecodeDTS(const BYTE * const p, int buffsize, int &consumed, BufferDetails *out);
+  HRESULT DecodeDTS(const BYTE * const p, int buffsize, int &consumed, HRESULT *hrDeliver);
+  int SafeDTSDecode(BYTE *pInput, int len, BYTE *pOutput, int unk1, int unk2, int *pBitdepth, int *pChannels, int *pCoreSampleRate, int *pUnk4, int *pHDSampleRate, int *pUnk5, int *pProfile);
 
   HRESULT CheckChannelLayoutConformity(DWORD dwLayout);
   HRESULT Create51Conformity(DWORD dwLayout);

commit b84f41693a4e555c2508a0e3ea35b7e11ef2e9d4

Properly send delivery failures upstream again.
This fixes a regression from a17c43c29c9d61a96abd93599cddb06a122ede7c

decoder/LAVAudio/DTSDecoder.cpp
@@ -397,6 +397,9 @@ HRESULT CLAVAudio::DecodeDTS(const BYTE * const p, int buffsize, int &consumed, 
 
       if (SUCCEEDED(PostProcess(&out))) {
         *hrDeliver = QueueOutput(out);
+        if (FAILED(*hrDeliver)) {
+          return S_FALSE;
+        }
       }
     }
   }

decoder/LAVAudio/LAVAudio.cpp
@@ -1384,6 +1384,8 @@ HRESULT CLAVAudio::ProcessBuffer(BOOL bEOF)
       m_buff.SetSize(0);
       m_bQueueResync = TRUE;
       return S_FALSE;
+    } else if (FAILED(hr)) {
+      DbgLog((LOG_TRACE, 10, L"::Decode indicates delivery failed"));
     } else if (hr2 == S_FALSE) {
       DbgLog((LOG_TRACE, 10, L"::Decode returned S_FALSE"));
       hr = S_FALSE;