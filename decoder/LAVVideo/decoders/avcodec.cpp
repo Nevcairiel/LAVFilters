fe8e38e1da880198e60c974b3185447cff69c39b
Hendrik Leppkes <h.leppkes@gmail.com>
2011-12-27 13:56:48 +0100

    Disable MPEG4-ASP multi-threaded decoding.
    Multi-threaded decoding causes a few playback glitches, especially after seeking on certain files, and the speed bonus from multi-threading is not crucial for a simple codec like MPEG4

--- decoder/LAVVideo/decoders/avcodec.cpp
+++ decoder/LAVVideo/decoders/avcodec.cpp
@@ -52,7 +52,6 @@ static struct {
   { CODEC_ID_VP3,        FF_THREAD_FRAME                 },
   { CODEC_ID_THEORA,     FF_THREAD_FRAME                 },
   { CODEC_ID_HUFFYUV,    FF_THREAD_FRAME                 },
-  { CODEC_ID_MPEG4,      FF_THREAD_FRAME                 },
   { CODEC_ID_PRORES,                     FF_THREAD_SLICE },
 };
 
