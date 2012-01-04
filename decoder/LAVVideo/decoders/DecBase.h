Nevcairiel/LAVFilters

commit c297d04b8223c9f033c4069f099c6fe6f67d1502

Allow decoders to take a IMediaSample instead of the values directly.

decoder/LAVVideo/decoders/DecBase.h
@@ -28,11 +28,33 @@ public:
   virtual ~CDecBase(void) {}
 
   STDMETHOD(Init)() PURE;
+  STDMETHOD(Decode)(const BYTE *buffer, int buflen, REFERENCE_TIME rtStart, REFERENCE_TIME rtStop, BOOL bSyncPoint, BOOL bDiscontinuity) { return E_NOTIMPL; }
 
   // ILAVDecoder
-  STDMETHODIMP InitInterfaces(ILAVVideoSettings *pSettings, ILAVVideoCallback *pCallback) { m_pSettings = pSettings; m_pCallback = pCallback; return Init(); };
+  STDMETHODIMP InitInterfaces(ILAVVideoSettings *pSettings, ILAVVideoCallback *pCallback) { m_pSettings = pSettings; m_pCallback = pCallback; return Init(); }
   STDMETHOD_(REFERENCE_TIME, GetFrameDuration)() { return 0; }
   STDMETHOD_(BOOL, IsInterlaced)() { return TRUE; }
+
+  STDMETHODIMP Decode(IMediaSample *pSample) {
+    HRESULT hr;
+
+    // Retrieve buffer
+    BYTE *pData = NULL;
+    if (FAILED(hr = pSample->GetPointer(&pData))) {
+      return hr;
+    }
+
+    // Retrieve timestamps
+    REFERENCE_TIME rtStart, rtStop;
+    pSample->GetTime(&rtStart, &rtStop);
+
+    if (FAILED(hr)) {
+      rtStart = rtStop = AV_NOPTS_VALUE;
+    } else if (hr == VFW_S_NO_STOP_TIME || rtStop-1 <= rtStart) {
+      rtStop = AV_NOPTS_VALUE;
+    }
+    return Decode(pData, pSample->GetActualDataLength(), rtStart, rtStop, pSample->IsSyncPoint() == S_OK, pSample->IsDiscontinuity() == S_OK);
+  }
 
 protected:
   // Convenience wrapper around m_pCallback

decoder/LAVVideo/LAVVideo.cpp
@@ -809,10 +809,6 @@ HRESULT CLAVVideo::Receive(IMediaSample *pIn)
 {
   CAutoLock cAutoLock(&m_csReceive);
   HRESULT        hr = S_OK;
-  BYTE           *pDataIn = NULL;
-  int            nSize;
-  REFERENCE_TIME rtStart = AV_NOPTS_VALUE;
-  REFERENCE_TIME rtStop = AV_NOPTS_VALUE;
 
   AM_SAMPLE2_PROPERTIES const *pProps = m_pInput->SampleProps();
   if(pProps->dwStreamId != AM_STREAM_MEDIA) {
@@ -827,22 +823,7 @@ HRESULT CLAVVideo::Receive(IMediaSample *pIn)
     DeleteMediaType(pmt);
   }
 
-  if (FAILED(hr = pIn->GetPointer(&pDataIn))) {
-    return hr;
-  }
-
-  nSize = pIn->GetActualDataLength();
-  hr = pIn->GetTime(&rtStart, &rtStop);
-
-  if (FAILED(hr)) {
-    rtStart = rtStop = AV_NOPTS_VALUE;
-  }
-
-  if (hr == VFW_S_NO_STOP_TIME || rtStop-1 <= rtStart) {
-    rtStop = AV_NOPTS_VALUE;
-  }
-
-  hr = m_pDecoder->Decode(pDataIn, nSize, rtStart, rtStop, pIn->IsSyncPoint() == S_OK, pIn->IsDiscontinuity() == S_OK);
+  hr = m_pDecoder->Decode(pIn);
   // If a hardware decoder indicates a hard failure, we switch back to software
   // This is used to indicate incompatible media
   if (FAILED(hr) && m_bHWDecoder) {
@@ -857,7 +838,7 @@ HRESULT CLAVVideo::Receive(IMediaSample *pIn)
     }
 
     DbgLog((LOG_TRACE, 10, L"-> Software decoder created, decoding frame again..."));
-    hr = m_pDecoder->Decode(pDataIn, nSize, rtStart, rtStop, pIn->IsSyncPoint() == S_OK, pIn->IsDiscontinuity() == S_OK);
+    hr = m_pDecoder->Decode(pIn);
   }
 
   return S_OK;

decoder/LAVVideo/decoders/ILAVDecoder.h
@@ -226,18 +226,13 @@ interface ILAVDecoder
   /**
    * Decode a frame.
    *
-   * @param buffer the input buffer containing the data to be decoded
-   * @param buflen length of the buffer, in bytes
-   * @param rtStart start time as delivered in the DirectShow input sample
-   * @param rtStop stop time as delivered in the DirectShow input sample
-   * @param bSyncPoint TRUE if the input sample indicated a sync point
-   * @param bDiscontinuity TRUE if the input sample indicated a discontinuity
+   * @param pSample Media Sample to decode
    * @return S_OK if decoding was successfull, S_FALSE if no frame could be extracted, an error code if the decoder is not compatible with the bitstream
    *
    * Note: When returning an actual error code, the filter will switch to the fallback software decoder! This should only be used for catastrophic failures,
    * like trying to decode a unsupported format on a hardware decoder.
    */
-  STDMETHOD(Decode)(const BYTE *buffer, int buflen, REFERENCE_TIME rtStart, REFERENCE_TIME rtStop, BOOL bSyncPoint, BOOL bDiscontinuity) PURE;
+  STDMETHOD(Decode)(IMediaSample *pSample) PURE;
 
   /**
    * Flush the decoder after a seek.

commit 21e98e39ad1c9001d5fe805ea1efe24a58391d57

Fix a timing error introduced by c297d04

decoder/LAVVideo/decoders/DecBase.h
@@ -46,7 +46,7 @@ public:
 
     // Retrieve timestamps
     REFERENCE_TIME rtStart, rtStop;
-    pSample->GetTime(&rtStart, &rtStop);
+    hr = pSample->GetTime(&rtStart, &rtStop);
 
     if (FAILED(hr)) {
       rtStart = rtStop = AV_NOPTS_VALUE;