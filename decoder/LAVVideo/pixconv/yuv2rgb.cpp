5fb5e68640fbd9feb2154d1b95b377f294ec2359
Hendrik Leppkes <h.leppkes@gmail.com>
2012-01-31 17:47:01 +0100

    pixconv: rgb32: refactor dithering coefficient creation.
    Because the RGB32 converter has two lines of pixels in one register, we
    have to take dithering coefficients for two lines and combine them,
    otherwise we risk the pattern to be visible.

--- decoder/LAVVideo/pixconv/yuv2rgb.cpp
+++ decoder/LAVVideo/pixconv/yuv2rgb.cpp
@@ -231,7 +231,18 @@ static int yuv2rgb_convert_pixels(const uint8_t* &srcY, const uint8_t* &srcU, co
   xmm1 = _mm_add_epi16(xmm1, xmm0);                           /* B (12bit) */
 
   // Dithering
-  PIXCONV_LOAD_DITHER_COEFFS(xmm2, line, 4, dithers);         /* Load dithering coeffs for 12->8 dithering */
+  /* Load dithering coeffs and combine them for two lines */
+  const uint16_t *dithers = dither_8x8_256[line % 8];
+  xmm2 = _mm_load_si128((const __m128i *)dithers);
+  const uint16_t *dithers2 = dither_8x8_256[(line+1) % 8];
+  xmm3 = _mm_load_si128((const __m128i *)dithers2);
+
+  xmm3 = _mm_srli_si128 (xmm3, 8);
+  xmm4 = _mm_set_epi32(0xffffffff, 0xffffffff, 0, 0);
+  xmm2 = _mm_and_si128(xmm2, xmm4);
+  xmm2 = _mm_or_si128(xmm2, xmm3);
+  xmm2 = _mm_srli_epi16(xmm2, 4);
+
   xmm6 = _mm_adds_epu16(xmm6, xmm2);                          /* Apply coefficients to the RGB values */
   xmm5 = _mm_adds_epu16(xmm5, xmm2);
   xmm1 = _mm_adds_epu16(xmm1, xmm2);
