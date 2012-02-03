a95be6bc377a639fd65aec65ac1aa2723be1df82
Hendrik Leppkes <h.leppkes@gmail.com>
2012-02-03 16:07:44 +0100

    Update QuickSync header file to 0.25

--- decoder/LAVVideo/decoders/quicksync/IQuickSyncDecoder.h
+++ decoder/LAVVideo/decoders/quicksync/IQuickSyncDecoder.h
@@ -29,7 +29,7 @@
 #pragma once
 
 #define QS_DEC_DLL_NAME "IntelQuickSyncDecoder.dll"
-#define QS_DEC_VERSION  "v0.24 Beta"
+#define QS_DEC_VERSION  "v0.25 Beta"
 
 // Forward declarations
 struct IDirect3DDeviceManager9;
