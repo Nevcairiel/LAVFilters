5234683a444539f30206544f717f82cb65b7f832
Hendrik Leppkes <h.leppkes@gmail.com>
2012-07-15 11:01:10 +0200

    Fix some typos

--- decoder/LAVAudio/AudioSettingsProp.cpp
+++ decoder/LAVAudio/AudioSettingsProp.cpp
@@ -441,12 +441,12 @@ HRESULT CLAVAudioMixingProp::OnActivate()
   SendDlgItemMessage(m_Dlg, IDC_MIX_LEVEL_LFE, TBM_SETRANGE, 0, MAKELONG(0, 10000));
   SendDlgItemMessage(m_Dlg, IDC_MIX_LEVEL_LFE, TBM_SETTICFREQ, 100, 0);
 
-  addHint(IDC_UNTOUCHED_STEREO, L"With this option on, stereo sources will not be mixed. This is useful when you want to mix all surround sources to eg. 5.1, but leave stereo untouched.");
+  addHint(IDC_UNTOUCHED_STEREO, L"With this option on, stereo sources will not be mixed. This is useful when you want to mix all surround sources to e.g. 5.1, but leave stereo untouched.");
   addHint(IDC_NORMALIZE_MATRIX, L"Normalizing the matrix will apply a global attenuation to the audio, in effect making it quieter to ensure that there is a consistent volume throughout the file, and no clipping occurs.\n\n"
-                                L"This mode will produce inconsistent volumes between different source formats (stereo will be louder then 5.1), but the volume during playback of one file not change.");
+                                L"This mode will produce inconsistent volumes between different source formats (stereo will be louder than 5.1), but the volume during playback of one file will not change.");
   addHint(IDC_CLIP_PROTECTION, L"Clipping protection analyzes the audio, and reduces the volume if clipping is detected.\n\n"
                                L"This mode tries to preserve the original volume of the audio, and is generally more consistent between different source formats. It may however cause a sudden volume change during playback. "
-                               L"In addition, this mode has a higher volume then a normalized matrix and is preferred on weak speakers or headphones.");
+                               L"In addition, this mode has a higher volume than a normalized matrix and is preferred on weak speakers or headphones.");
 
   hr = LoadData();
   if (SUCCEEDED(hr)) {
