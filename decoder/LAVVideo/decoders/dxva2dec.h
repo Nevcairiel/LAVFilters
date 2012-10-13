366dd9b1751e95d4b06d78104dd160433e3123ab
Hendrik Leppkes <h.leppkes@gmail.com>
2012-10-13 23:13:45 +0200

    Disable thread-safe buffers with DXVA2-CB, no advantage for hardware codecs.

--- decoder/LAVVideo/decoders/dxva2dec.h
+++ decoder/LAVVideo/decoders/dxva2dec.h
@@ -52,7 +52,6 @@ public:
   STDMETHODIMP PostConnect(IPin *pPin);
   STDMETHODIMP_(long) GetBufferCount();
   STDMETHODIMP_(const WCHAR*) GetDecoderName() { return m_bNative ? L"dxva2n" : L"dxva2cb"; }
-  STDMETHODIMP HasThreadSafeBuffers() { return m_bNative ? S_FALSE : S_OK; }
 
   // CDecBase
   STDMETHODIMP Init();
