457508c4630645464c0e377f378b50700cbfe343
Hendrik Leppkes <h.leppkes@gmail.com>
2012-12-07 09:33:39 +0100

    Always calculate all fps times for comparison purposes and simpler code.

--- demuxer/Demuxers/LAVFVideoHelper.cpp
+++ demuxer/Demuxers/LAVFVideoHelper.cpp
@@ -158,12 +158,23 @@ VIDEOINFOHEADER *CLAVFVideoHelper::CreateVIH(const AVStream* avstream, ULONG *si
   if (!pvi) return NULL;
   memset(pvi, 0, sizeof(VIDEOINFOHEADER));
   // Get the frame rate
-  REFERENCE_TIME r_avg, avg_avg, tb_avg;
-  if (avstream->r_frame_rate.den > 0 &&  avstream->r_frame_rate.num > 0 && ((r_avg = av_rescale(DSHOW_TIME_BASE, avstream->r_frame_rate.den, avstream->r_frame_rate.num)) > MIN_TIME_PER_FRAME) && r_avg < MAX_TIME_PER_FRAME) {
+  REFERENCE_TIME r_avg = 0, avg_avg = 0, tb_avg = 0;
+  if (avstream->r_frame_rate.den > 0 &&  avstream->r_frame_rate.num > 0) {
+    r_avg = av_rescale(DSHOW_TIME_BASE, avstream->r_frame_rate.den, avstream->r_frame_rate.num);
+  }
+  if (avstream->avg_frame_rate.den > 0 &&  avstream->avg_frame_rate.num > 0) {
+    avg_avg = av_rescale(DSHOW_TIME_BASE, avstream->avg_frame_rate.den, avstream->avg_frame_rate.num);
+  }
+  if (avstream->codec->time_base.den > 0 &&  avstream->codec->time_base.num > 0 && avstream->codec->ticks_per_frame > 0) {
+    tb_avg = av_rescale(DSHOW_TIME_BASE, avstream->codec->time_base.num * avstream->codec->ticks_per_frame, avstream->codec->time_base.den);
+  }
+
+  DbgLog((LOG_TRACE, 10, L"CreateVIH: r_avg: %I64d, avg_avg: %I64d, tb_avg: %I64d", r_avg, avg_avg, tb_avg));
+  if (r_avg >= MIN_TIME_PER_FRAME && r_avg <= MAX_TIME_PER_FRAME)
     pvi->AvgTimePerFrame = r_avg;
-  } else if (avstream->avg_frame_rate.den > 0 &&  avstream->avg_frame_rate.num > 0 && ((avg_avg = av_rescale(DSHOW_TIME_BASE, avstream->avg_frame_rate.den, avstream->avg_frame_rate.num)) > MIN_TIME_PER_FRAME) && avg_avg < MAX_TIME_PER_FRAME) {
+  else if (avg_avg >= MIN_TIME_PER_FRAME && avg_avg <= MAX_TIME_PER_FRAME)
     pvi->AvgTimePerFrame = avg_avg;
-  } else if (avstream->codec->time_base.den > 0 &&  avstream->codec->time_base.num > 0 && avstream->codec->ticks_per_frame > 0 && ((tb_avg = av_rescale(DSHOW_TIME_BASE, avstream->codec->time_base.num * avstream->codec->ticks_per_frame, avstream->codec->time_base.den)) > MIN_TIME_PER_FRAME) && tb_avg < MAX_TIME_PER_FRAME) {
+  else if (tb_avg >= MIN_TIME_PER_FRAME && tb_avg <= MAX_TIME_PER_FRAME)
     pvi->AvgTimePerFrame = tb_avg;
   }
   pvi->dwBitErrorRate = 0;
