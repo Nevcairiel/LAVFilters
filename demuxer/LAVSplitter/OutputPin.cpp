1290067be7a30f9363aa6bdf2e20188a8e4d81da
Hendrik Leppkes <h.leppkes@gmail.com>
2013-02-03 14:06:19 +0100

    Increase audio queue size for PCM in MKV

--- demuxer/LAVSplitter/OutputPin.cpp
+++ demuxer/LAVSplitter/OutputPin.cpp
@@ -61,7 +61,7 @@ void CLAVOutputPin::SetQueueSizes()
   if (m_mts.begin()->subtype == MEDIASUBTYPE_DOLBY_TRUEHD) {
     DbgLog((LOG_TRACE, 10, L"Increasing Audio Queue size for TrueHD"));
     factor = 35;
-  } else if (m_mts.begin()->subtype == MEDIASUBTYPE_HDMV_LPCM_AUDIO || m_mts.begin()->subtype == MEDIASUBTYPE_BD_LPCM_AUDIO) {
+  } else if (m_mts.begin()->subtype == MEDIASUBTYPE_HDMV_LPCM_AUDIO || m_mts.begin()->subtype == MEDIASUBTYPE_BD_LPCM_AUDIO || m_mts.begin()->subtype == MEDIASUBTYPE_PCM) {
     factor = 6;
   } else if (m_mts.begin()->majortype == MEDIATYPE_Audio) {
     factor = 3;
