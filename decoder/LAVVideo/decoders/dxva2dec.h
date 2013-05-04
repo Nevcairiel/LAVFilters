a7ae453627181bef7c4a47a1259c74829e3b1049
Hendrik Leppkes <h.leppkes@gmail.com>
2013-05-04 13:19:30 +0200

    dxva2: ensure we sync to the process thread if required

--- decoder/LAVVideo/decoders/dxva2dec.h
+++ decoder/LAVVideo/decoders/dxva2dec.h
@@ -52,6 +52,7 @@ public:
   STDMETHODIMP_(long) GetBufferCount();
   STDMETHODIMP_(const WCHAR*) GetDecoderName() { return m_bNative ? L"dxva2n" : L"dxva2cb"; }
   STDMETHODIMP HasThreadSafeBuffers() { return m_bNative ? S_FALSE : S_OK; }
+  STDMETHODIMP SyncToProcessThread() { return HasThreadSafeBuffers() == S_OK ? S_FALSE : S_OK; }
 
   // CDecBase
   STDMETHODIMP Init();
