84e54564db32ec937a7b326f9cee0735c1c19cb5
Hendrik Leppkes <h.leppkes@gmail.com>
2013-05-11 18:55:51 +0200

    Set appropriate PE flags

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -76,7 +76,7 @@ configure() (
     --arch=${arch}"
 
   EXTRA_CFLAGS="-D_WIN32_WINNT=0x0502 -DWINVER=0x0502 -I../thirdparty/include -idirafter../common/includes/dxva2"
-  EXTRA_LDFLAGS=""
+  EXTRA_LDFLAGS="-Wl,--nxcompat -Wl,--dynamicbase -Wl,--no-seh"
   if [ "${arch}" == "x86_64" ]; then
     OPTIONS="${OPTIONS} --enable-cross-compile --cross-prefix=x86_64-w64-mingw32- --target-os=mingw32"
     EXTRA_LDFLAGS="${EXTRA_LDFLAGS} -L../thirdparty/lib64"
