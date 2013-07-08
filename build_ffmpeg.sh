678c64760995f1f7099579b86f5aa7ac65d4ed1e
Hendrik Leppkes <h.leppkes@gmail.com>
2013-07-08 09:46:02 +0200

    Remove nxcompat/dynamicbase LDFLAGS
    They are set by ffmpeg configure automatically now.

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -85,7 +85,7 @@ configure() (
     --arch=${arch}"
 
   EXTRA_CFLAGS="-D_WIN32_WINNT=0x0502 -DWINVER=0x0502 -I../thirdparty/include -idirafter../common/includes/dxva2 -DPTW32_STATIC_LIB"
-  EXTRA_LDFLAGS="-Wl,--nxcompat -Wl,--dynamicbase"
+  EXTRA_LDFLAGS=""
   if [ "${arch}" == "x86_64" ]; then
     OPTIONS="${OPTIONS} --enable-cross-compile --cross-prefix=x86_64-w64-mingw32- --target-os=mingw32"
     EXTRA_LDFLAGS="${EXTRA_LDFLAGS} -L../thirdparty/lib64"
