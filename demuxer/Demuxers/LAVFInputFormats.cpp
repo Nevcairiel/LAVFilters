87d10d87dd8f15e8c100f9e10647e64b41229da1
Hendrik Leppkes <h.leppkes@gmail.com>
2013-10-30 13:54:47 +0100

    Map "hevc" to the rawvideo type

--- demuxer/Demuxers/LAVFInputFormats.cpp
+++ demuxer/Demuxers/LAVFInputFormats.cpp
@@ -42,6 +42,7 @@ struct lavf_iformat_map
   { "h261",  RAW_VIDEO, RAW_VIDEO_DESC },
   { "h263",  RAW_VIDEO, RAW_VIDEO_DESC },
   { "h264",  RAW_VIDEO, RAW_VIDEO_DESC },
+  { "hevc",  RAW_VIDEO, RAW_VIDEO_DESC },
   { "ingenient", RAW_VIDEO, RAW_VIDEO_DESC },
   { "mjpeg", RAW_VIDEO, RAW_VIDEO_DESC },
   { "vc1",   RAW_VIDEO, RAW_VIDEO_DESC },
