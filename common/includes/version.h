905f6b3bea683d21cbabbb2b1d0056fec3703803
Hendrik Leppkes <h.leppkes@gmail.com>
2013-11-03 16:15:42 +0100

    Update changelog and version

--- common/includes/version.h
+++ common/includes/version.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define LAV_VERSION_MAJOR     0
-#define LAV_VERSION_MINOR    58
-#define LAV_VERSION_REVISION  2
+#define LAV_VERSION_MINOR    59
+#define LAV_VERSION_REVISION  0
 
 #define LAV_AUDIO "LAV Audio Decoder"
 #define LAV_VIDEO "LAV Video Decoder"
