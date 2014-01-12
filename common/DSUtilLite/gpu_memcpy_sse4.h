245dda9053063e41e16c3ed19094aee273bbd62d
Hendrik Leppkes <h.leppkes@gmail.com>
2014-01-12 16:15:12 +0100

    Update copyright year to 2014

--- common/DSUtilLite/gpu_memcpy_sse4.h
+++ common/DSUtilLite/gpu_memcpy_sse4.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2011-2013 Hendrik Leppkes
+ *      Copyright (C) 2011-2014 Hendrik Leppkes
  *      http://www.1f0.de
  *
  *  This program is free software; you can redistribute it and/or modify
--- decoder/LAVAudio/LAVAudioSettings.h
+++ decoder/LAVAudio/LAVAudioSettings.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2010-2013 Hendrik Leppkes
+ *      Copyright (C) 2010-2014 Hendrik Leppkes
  *      http://www.1f0.de
  *
  *  This program is free software; you can redistribute it and/or modify
