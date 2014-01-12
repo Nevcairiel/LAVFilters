8d618c392ed0520d1c4061cb2779be5fe87d5a62
Hendrik Leppkes <h.leppkes@gmail.com>
2014-01-12 16:16:46 +0100

    Update developer_info

---
+++ developer_info/ISpecifyPropertyPages2.h
@@ -0,0 +1,29 @@
+/*
+ * (C) 2003-2006 Gabest
+ * (C) 2006-2010 MPC-HC Authors
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#pragma once
+
+#include <OCIdl.h>
+
+interface __declspec(uuid("03481710-D73E-4674-839F-03EDE2D60ED8"))
+ISpecifyPropertyPages2 : public ISpecifyPropertyPages {
+	STDMETHOD (CreatePage) (const GUID& guid, IPropertyPage** ppPage) = 0;
+};
