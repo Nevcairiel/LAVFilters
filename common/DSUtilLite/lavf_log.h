64d1e32e5a407756e81d097f7a2e2ae9450e4089
Hendrik Leppkes <h.leppkes@gmail.com>
2014-10-24 11:26:00 +0200

    Improve ff debug logging

--- common/DSUtilLite/lavf_log.h
+++ common/DSUtilLite/lavf_log.h
@@ -36,11 +36,12 @@ inline void lavf_log_callback(void* ptr, int level, const char* fmt, va_list vl)
     return;
   }
   if(count>0){
-    DbgLog((LOG_CUSTOM1, level, L"    Last message repeated %d times\n", count));
+    DbgLog((LOG_CUSTOM1, level, L"    Last message repeated %d times", count));
     count=0;
   }
-  if (line[strlen(line)-1] == '\n') {
-    line[strlen(line)-1] = 0;
+  int len = strnlen_s(line, LOG_BUF_LEN);
+  if (len > 0 && line[len - 1] == '\n') {
+    line[len - 1] = 0;
   }
 
   DbgLog((LOG_CUSTOM1, level, L"%S", line));
