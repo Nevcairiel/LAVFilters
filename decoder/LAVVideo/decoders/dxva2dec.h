cd9344ce6de708d02beb9e88304ba73639ca55a3
Hendrik Leppkes <h.leppkes@gmail.com>
2015-01-03 12:56:32 +0100

    Happy new Year!

--- decoder/LAVVideo/decoders/dxva2dec.h
+++ decoder/LAVVideo/decoders/dxva2dec.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2011-2014 Hendrik Leppkes
+ *      Copyright (C) 2011-2015 Hendrik Leppkes
  *      http://www.1f0.de
  *
  *  This program is free software; you can redistribute it and/or modify
