a616d2bd0e761cf4d1150218db40d92d26b7dc1d
Hendrik Leppkes <h.leppkes@gmail.com>
2015-01-04 11:37:28 +0100

    build.bat: stop execution if one step failed

--- build.bat
+++ build.bat
@@ -2,10 +2,10 @@
 
 call "%VS120COMNTOOLS%vsvars32.bat"
 
-sh build_ffmpeg.sh x86
-devenv LAVFilters.sln /Rebuild "Release|Win32"
+sh build_ffmpeg.sh x86 || EXIT /B 1
+devenv LAVFilters.sln /Rebuild "Release|Win32" || EXIT /B 1
 
-sh build_ffmpeg.sh x64
-devenv LAVFilters.sln /Rebuild "Release|x64"
+sh build_ffmpeg.sh x64 || EXIT /B 1
+devenv LAVFilters.sln /Rebuild "Release|x64" || EXIT /B 1
 
 PAUSE
