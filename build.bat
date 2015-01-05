f9e1f1117d5e910d78e563f7bcbed32793b15335
Hendrik Leppkes <h.leppkes@gmail.com>
2015-01-06 00:26:34 +0100

    Use MSBuild instead of devenv in build script
    MSBuild is available in VS Express, while devenv is not.

--- build.bat
+++ build.bat
@@ -3,9 +3,11 @@
 call "%VS120COMNTOOLS%vsvars32.bat"
 
 sh build_ffmpeg.sh x86 || EXIT /B 1
-devenv LAVFilters.sln /Rebuild "Release|Win32" || EXIT /B 1
+MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=Win32
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
 
 sh build_ffmpeg.sh x64 || EXIT /B 1
-devenv LAVFilters.sln /Rebuild "Release|x64" || EXIT /B 1
+MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=x64
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
 
 PAUSE
