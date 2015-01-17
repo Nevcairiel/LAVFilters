c4fceb85b199a2b33e1e188f07387348a6a5ce8d
Hendrik Leppkes <h.leppkes@gmail.com>
2015-01-17 14:32:32 +0100

    dxva2: use surface dimensions when probing the decoder as well

--- decoder/LAVVideo/decoders/dxva2dec.cpp
+++ decoder/LAVVideo/decoders/dxva2dec.cpp
@@ -763,8 +763,8 @@ HRESULT CDecDXVA2::SetD3DDeviceManager(IDirect3DDeviceManager9 *pDevManager)
 
     DXVA2_VideoDesc desc;
     ZeroMemory(&desc, sizeof(desc));
-    desc.SampleWidth = m_pAVCtx->coded_width;
-    desc.SampleHeight = m_pAVCtx->coded_height;
+    desc.SampleWidth = m_dwSurfaceWidth;
+    desc.SampleHeight = m_dwSurfaceHeight;
     desc.Format = output;
 
     DXVA2_ConfigPictureDecode config;
