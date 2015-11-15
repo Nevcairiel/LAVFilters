90b6ae3ff3b2c5ad78b88f9d6e43f999db32fdf5
Hendrik Leppkes <h.leppkes@gmail.com>
2015-11-15 14:16:28 +0100

    Use the /Zo switch instead of the deprecated and undocumented variant
    Zo was added in VS2013 Update 3 as an official replacement for /d2Zi+

--- build_ffmpeg_msvc.sh
+++ build_ffmpeg_msvc.sh
@@ -79,7 +79,7 @@ configure() (
     --build-suffix=-lav             \
     --arch=${arch}"
 
-  EXTRA_CFLAGS="-D_WIN32_WINNT=0x0502 -DWINVER=0x0502 -d2Zi+ -GS-"
+  EXTRA_CFLAGS="-D_WIN32_WINNT=0x0502 -DWINVER=0x0502 -Zo -GS-"
   EXTRA_LDFLAGS=""
 
   if $debug ; then
