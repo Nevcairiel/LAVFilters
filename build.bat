78754d884da1794af339254245b7d79dcf33ea94
Hendrik Leppkes <h.leppkes@gmail.com>
2017-08-11 20:03:55 +0200

    Update build script to use VS2017 by default

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
 @ECHO OFF
 
-call "%VS140COMNTOOLS%vsvars32.bat"
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set VSInstallDir=%%i
+)
+
+set "VSCMD_START_DIR=%CD%"
+call "%VSInstallDir%\Common7\Tools\VsDevCmd.bat" -no_logo -arch=x86 > NUL
 
 sh build_ffmpeg.sh x86 || EXIT /B 1
 MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=Win32
