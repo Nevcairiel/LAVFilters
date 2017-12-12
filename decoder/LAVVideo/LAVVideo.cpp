aab883bd37468313d3897a9236262d1efe592ac5
Hendrik Leppkes <h.leppkes@gmail.com>
2017-12-12 11:03:58 +0100

    Flush data out of the decoder in NewSegment instead of discarding it
    This allows the source filter to call NewSegment on actual segment
    changes, and get a seamless cut. On Seeking, Begin/EndFlush will instead
    flush the decoder.

--- decoder/LAVVideo/LAVVideo.cpp
+++ decoder/LAVVideo/LAVVideo.cpp
@@ -813,14 +813,11 @@ HRESULT CLAVVideo::EndFlush()
   DbgLog((LOG_TRACE, 1, L"::EndFlush"));
   CAutoLock cAutoLock(&m_csReceive);
 
-  ReleaseLastSequenceFrame();
-
-  if (m_dwDecodeFlags & LAV_VIDEO_DEC_FLAG_DVD) {
-    PerformFlush();
-  }
+  PerformFlush();
 
   HRESULT hr = __super::EndFlush();
   m_bFlushing = FALSE;
+  m_bFlushed = TRUE;
   return hr;
 }
 
@@ -864,7 +861,14 @@ HRESULT CLAVVideo::NewSegment(REFERENCE_TIME tStart, REFERENCE_TIME tStop, doubl
 {
   DbgLog((LOG_TRACE, 1, L"::NewSegment - %I64d / %I64d", tStart, tStop));
 
-  PerformFlush();
+  // If this call is not directly following a flush anyway..
+  if (m_bFlushing == FALSE && m_bFlushed == FALSE) {
+    // Ensure all previous data is flushed out of the decoder
+    m_Decoder.EndOfStream();
+
+    // and reset the decoder
+    PerformFlush();
+  }
 
   return __super::NewSegment(tStart, tStop, dRate);
 }
@@ -1443,6 +1447,7 @@ HRESULT CLAVVideo::Receive(IMediaSample *pIn)
   }
 
   m_hrDeliver = S_OK;
+  m_bFlushed = FALSE;
 
   // Skip over empty packets
   if (pIn->GetActualDataLength() == 0) {
