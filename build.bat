164112610f2adf98c96835a470e2d68987c966e7
Hendrik Leppkes <h.leppkes@gmail.com>
2018-09-06 16:03:36 +0200

    Change errorlevel check to work properly again

--- build.bat
+++ build.bat
@@ -9,10 +9,10 @@ call "%VSInstallDir%\Common7\Tools\VsDevCmd.bat" -no_logo -arch=x86 > NUL
 
 sh build_ffmpeg.sh x86 || EXIT /B 1
 MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=Win32
-IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+IF ERRORLEVEL 1 EXIT /B 1
 
 sh build_ffmpeg.sh x64 || EXIT /B 1
 MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=x64
-IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+IF ERRORLEVEL 1 EXIT /B 1
 
 PAUSE
