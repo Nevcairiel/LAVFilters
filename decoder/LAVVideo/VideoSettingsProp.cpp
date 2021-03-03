defe85d321b94a11ebb6273e2bd827e008e2be0b
Hendrik Leppkes <h.leppkes@gmail.com>
2021-03-03 13:56:46 +0100

    Move CUDA and QuickSync hwaccels to the bottom of the list and mark them out of date

--- decoder/LAVVideo/VideoSettingsProp.cpp
+++ decoder/LAVVideo/VideoSettingsProp.cpp
@@ -248,17 +248,17 @@ HRESULT CLAVVideoSettingsProp::OnActivate()
                           L"(Auto) - Stream AR will not be used on files with a container AR (recommended).");
 
     WCHAR hwAccelNone[] = L"None";
-    WCHAR hwAccelCUDA[] = L"NVIDIA CUVID";
-    WCHAR hwAccelQuickSync[] = L"Intel\xae QuickSync";
+    WCHAR hwAccelCUDA[] = L"NVIDIA CUVID (old)";
+    WCHAR hwAccelQuickSync[] = L"Intel\xae QuickSync (old)";
     WCHAR hwAccelDXVA2CB[] = L"DXVA2 (copy-back)";
     WCHAR hwAccelDXVA2N[] = L"DXVA2 (native)";
     WCHAR hwAccelD3D11[] = L"D3D11";
     AddComboBoxEntryWithData(m_Dlg, IDC_HWACCEL, hwAccelNone, HWAccel_None);
-    AddComboBoxEntryWithData(m_Dlg, IDC_HWACCEL, hwAccelCUDA, HWAccel_CUDA);
-    AddComboBoxEntryWithData(m_Dlg, IDC_HWACCEL, hwAccelQuickSync, HWAccel_QuickSync);
     AddComboBoxEntryWithData(m_Dlg, IDC_HWACCEL, hwAccelDXVA2CB, HWAccel_DXVA2CopyBack);
     AddComboBoxEntryWithData(m_Dlg, IDC_HWACCEL, hwAccelDXVA2N, HWAccel_DXVA2Native);
     AddComboBoxEntryWithData(m_Dlg, IDC_HWACCEL, hwAccelD3D11, HWAccel_D3D11);
+    AddComboBoxEntryWithData(m_Dlg, IDC_HWACCEL, hwAccelCUDA, HWAccel_CUDA);
+    AddComboBoxEntryWithData(m_Dlg, IDC_HWACCEL, hwAccelQuickSync, HWAccel_QuickSync);
 
     // Init the fieldorder Combo Box
     SendDlgItemMessage(m_Dlg, IDC_DEINT_FIELDORDER, CB_RESETCONTENT, 0, 0);
