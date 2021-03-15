b7129ea1752801b31e21cc58b45d2617fb2355ca
Hendrik Leppkes <h.leppkes@gmail.com>
2021-03-15 10:23:41 +0100

    Update copyright year

--- decoder/LAVVideo/decoders/dxva2/dxva_common.h
+++ decoder/LAVVideo/decoders/dxva2/dxva_common.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2011-2019 Hendrik Leppkes
+ *      Copyright (C) 2011-2021 Hendrik Leppkes
  *      http://www.1f0.de
  *
  *  This program is free software; you can redistribute it and/or modify
