85286c1b6caf78cdcfd738d43354f9240a7eedff
Hendrik Leppkes <h.leppkes@gmail.com>
2025-06-17 17:28:01 +0200

    video: add new media types

--- include/LAVVideoSettings.h
+++ include/LAVVideoSettings.h
@@ -90,6 +90,8 @@ typedef enum LAVVideoCodec
     Codec_MagicYUV,
     Codec_AV1,
     Codec_VVC,
+    Codec_VP4,
+    Codec_FIC,
 
     Codec_VideoNB // Number of entries (do not use when dynamically linking)
 } LAVVideoCodec;
